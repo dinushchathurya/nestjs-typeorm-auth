120ef030457019291e3c9bdba813df91708c9e45
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-22 21:14:08 +0530

    :wrench: Configure ormconfig.ts in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import config from 'ormconfig';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: entities,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(config),
     UserModule,
   ],
   controllers: [AppController],
